5ab917b05ad324791b6aac956fe453a003c7de01
Sean Helvey <smh436@nyu.edu>
2012-02-21 18:45:52 -0500

    Added a sweet comment

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -13,6 +13,7 @@ namespace WindowsFormsApplication1
         [STAThread]
         static void Main()
         {
+            //Sweet Comment
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
